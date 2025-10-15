56e393b33bab4a767a81ed1ce65f26de8260200f
engrassist-svg <engrassist@gmail.com>
2025-10-14 22:56:32 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'engrassist-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/scripts.js',
+  '/header.html',
+  '/footer.html',
+  '/mechanical_page.html',
+  '/ductulator.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
